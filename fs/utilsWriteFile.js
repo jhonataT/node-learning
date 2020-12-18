e5769cf64b33aa611d75bbb0d0f6edfac777fdc9
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-18 11:40:26 -0300

    util

--- fs/utilsWriteFile.js
+++ fs/utilsWriteFile.js
@@ -1,3 +1,12 @@
-const { primisefy } = require('util');
+const { promisify } = require('util');
 
-const writeFile = 
+const writeFile = promisify(require('fs').writeFile); // carregar o fs para utilizar promises.
+const text = 'Criando um arquivo utilizando promisify do módulo nativo util';
+
+writeFile('utilArquivo.txt', text)
+.then( () => {
+    console.log('Sucess');
+})
+.catch( err => {
+    console.log(err);
+})
